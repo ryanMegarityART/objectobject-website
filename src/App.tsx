f4c7e65301eda22ef012f187431681ef9c879f70
ryanMegarityART <ryan.megarity@pshealthgroup.com>
2022-07-28 21:47:05 +0100

    embed player

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.jpg';
-import './App.css';
+import React from "react";
+import logo from "./logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         __init__
-        </p>
-        <a
-          className="App-link"
-          href="https://soundcloud.com/birdgang4ever/object-object-live-minimix-1"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Listen to the minimix
-        </a>
+        <p>__init__</p>
+        <iframe
+          width="100%"
+          height="166"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https://soundcloud.com/birdgang4ever/object-object-live-minimix-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+        ></iframe>
       </header>
     </div>
   );
